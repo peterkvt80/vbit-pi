5e30d8b41c5367a942b488ced5f8ccd7c2d410b9
peterk.vt80@gmail.com <peterk.vt80@gmail.com@61eccfb8-4210-702b-6a43-dd5d631e1ac7>
2015-02-14 12:12:58 +0000

    First stream version

---
+++ tags/stream/outputstream.c
@@ -0,0 +1,23 @@
+#include "outputstream.h"
+
+char mydata[45];
+
+PI_THREAD (OutputStream)
+{
+	int j;
+	// printf("OutputStream thread started\n");
+	while(1)
+	{
+		// printf("OutputStream thread continues\n");
+
+		// Loop if we have a buffer under-run
+		while (bufferGet(streamBuffer,mydata))
+		{
+			delay(10);
+		}
+
+		fwrite(&mydata[3],1,42,stdout);
+			// printf("%c",mydata[j]);
+		//	printf("[FillFIFO] GOT STUFF !!!!!!!! bufferGet \n");
+	}
+}
